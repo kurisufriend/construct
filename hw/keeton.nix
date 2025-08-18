69badef5f52fb9505fc3c4506b5d54074d097808
cynic <admin@cynic.moe>
2025-08-18 01:53:54 -0400

    sshd: keyonly etc. for deploys

--- hw/keeton.nix
+++ hw/keeton.nix
@@ -4,6 +4,7 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
       ../domains/common.nix
+      ../domains/sshd.nix
       ../domains/desktop.nix
       ../domains/laptop.nix
       ../domains/mounts.nix
