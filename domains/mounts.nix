274b6fc2b34eaa70ca0f0ecf1e84143aaaf533ea
cynic <admin@cynic.moe>
2025-10-18 19:45:45 -0400

    domains/mounts: add rlogin

--- domains/mounts.nix
+++ domains/mounts.nix
@@ -7,9 +7,26 @@
     host = dirtycow.vtluug.org
     user = rsk
     key_file = /home/rsk/.ssh/id_ed25519
+
+    [rlogin]
+    type = sftp
+    host = rlogin.cs.vt.edu
+    user = rishik
+    key_file = /home/rsk/.ssh/id_ed25519
 '';
   fileSystems."/mnt/cistern" = {
     device = "dirtycow:/cistern";
+    fsType = "rclone";
+    options = [
+      "nodev"
+      "nofail"
+      "allow_other"
+      "args2env"
+      "config=/etc/rclone-mnt.conf"
+    ];
+  };
+  fileSystems."/mnt/rlogin" = {
+    device = "rlogin:/home/ugrads/majors/rishik";
     fsType = "rclone";
     options = [
       "nodev"
