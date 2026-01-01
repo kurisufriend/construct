61ffa8d69480a1e9e4393ffa3929eb7fc3a48566
cynic <admin@cynic.moe>
2025-12-31 19:22:32 -0500

    hosts/imp: run as desktop 4 nao

--- hosts/imp/configuration.nix
+++ hosts/imp/configuration.nix
@@ -2,7 +2,13 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix ./overrides.nix ./overrides.nix ./overrides.nix ./overrides.nix ./overrides.nix
+      ./hardware-configuration.nix ./overrides.nix
+      ../../roles/users.nix
+      ../../roles/common.nix
+      ../../roles/sshd.nix
+      ../../roles/desktop.nix
+      ../../roles/tailscale.nix
+      ../../roles/ygg-client.nix
     ];
   networking.networkmanager.enable = true;
   system.stateVersion = "25.11"; # Did you read the comment?
